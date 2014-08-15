2d0f7c3d79defa1fb16bbeb859d30452ab314738
Maciej Trębacz <maciej@trebacz.org>
2014-08-15 13:11:47 +0200

    Add refresh token functionality to Auth API

--- lib/BitaloApi.php
+++ lib/BitaloApi.php
@@ -7,11 +7,30 @@ class BitaloApi
     const API_URL = "https://bitalo.com/api/1";
     const AUTH_URL = "https://bitalo.com/auth";
 
+    // Error codes from API
+    const ERROR_AUTH_DENIED = 21;
+    const ERROR_AUTH_CODE_INVALID = 22;
+    const ERROR_AUTH_CLIENT_INVALID = 23;
+    const ERROR_AUTH_TOKEN_INVALID = 24;
+    const ERROR_AUTH_TOKEN_EXPIRED = 25;
+    const ERROR_AUTH_FAILED = 26;
+    const ERROR_API = 27;
+    const ERROR_REFRESH_TOKEN_INVALID = 28;
+
     // User Agent string for sending requests
     public $ua_string = "BitaloApi/v1.0";
 
     // Timeout for API requests
     public $request_timeout = 10;
+
+    // If the class should try to refresh tokens automatically
+    public $auto_token_refresh = true;
+
+    // Function called upon token refresh
+    public $refresh_callback;
+
+    // Client (application) ID
+    private $client_id = "";
 
     // Client secret used to authenticate requests
     private $client_secret = "";
@@ -21,6 +40,9 @@ class BitaloApi
 
     // Currently used access token
     private $access_token;
+
+    // Currently used refresh token
+    private $refresh_token;
 
     /**
      * Bitalo API class constructor
@@ -42,10 +64,11 @@ class BitaloApi
      * Makes a request to Bitalo API
      * @param string $method HTTP method, one of: GET, POST, PUT, DELETE
      * @param string $url URL to be requested
-     * @param array $data Data to be sent
+     * @param array $data Data to be sent (optional)
+     * @param boolean $resent Whether the request was resent due to expired token
      * @returns string parsed JSON response
      */
-    public function request($method, $url, $data = array()) 
+    public function request($method, $url, $data = array(), $resent = false) 
     {
         // Check if method is allowed
         $allowed_methods = ["GET", "POST", "PUT", "DELETE"];
@@ -80,9 +103,9 @@ class BitaloApi
         $curl_options[CURLOPT_HTTPHEADER] = $http_headers;
 
         // Build URL depending on method and data
-        $url = $this->buildURL($method, $url, $data);
+        $request_url = $this->buildURL($method, $url, $data);
         $query = http_build_query($data);
-        $curl_options[CURLOPT_URL] = $url;
+        $curl_options[CURLOPT_URL] = $request_url;
 
         // Handle different HTTP methods
         switch ($method) {
@@ -125,15 +148,40 @@ class BitaloApi
                 BitaloApi_Exception::REQUEST_DECODE);
         }
 
+        // If we get a "token expired" response and token auto refresh is enabled
+        // we issue a refresh token request and re-send the original request. If that fails again,
+        // no further attempts are made. A refresh callback function has to be defined before.
+        if ($json->status == "error" && $json->code == BitaloApi::ERROR_AUTH_TOKEN_EXPIRED
+            && $this->auto_token_refresh == true && isset($this->refresh_token) && !$resent
+            && isset($this->refresh_callback) && is_callable($this->refresh_callback)) {
+            curl_close($curl);
+
+            $this->refreshTokens();
+            return $this->request($method, $url, $data, TRUE);
+        }
+
         // Close the curl handle and return the data
         curl_close($curl);
         return $json;
     }
 
     /**
+     * Stores the tokens for later requests
+     * @param string $access_token Access token
+     * @param string $refresh_token Refresh token (optional)
+     */
+    public function setTokens($access_token, $refresh_token = "")
+    {
+        $this->access_token = $access_token;
+        if ($refresh_token) {
+            $this->refresh_token = $refresh_token;
+        }
+    }
+
+    /**
      * Fetches access token in exchange for authorization code
      * @param string $code Authorization code
-     * @returns array access_token, expires_in, token_type, scope, user_id
+     * @returns array access_token, refresh_token, expires_in, token_type, scope, user_id
      */
     public function getAuthToken($code) 
     {
@@ -145,11 +193,49 @@ class BitaloApi
         ]);
 
         if ($response && $response->status == "ok") {
-            $this->access_token = $response->access_token;
+            $this->setTokens($response->access_token, $response->refresh_token);
         }
         else {
             throw new BitaloApi_Exception("Getting token failed. Server response: " . 
                 print_r($response, true), BitaloApi_Exception::AUTH_ERROR);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Fetches a fresh access token in exchange for refresh token
+     * @param string $refresh_token Refresh token (optional if already set in instance variable)
+     * @returns array access_token, refresh_token, expires_in, token_type, scope, user_id
+     */
+    public function refreshTokens($refresh_token = "")
+    {
+        // Check for refresh token
+        if (!$refresh_token) {
+            if (isset($this->refresh_token)) {
+                $refresh_token = $this->refresh_token;
+            }
+            else {
+                throw new BitaloApi_Exception("Refresh token is required for calling refreshTokens()", 
+                    BitaloApi_Exception::TOKEN_MISSING);
+            }
+        }
+
+        $response = $this->request("POST", self::AUTH_URL . "/token/", [
+            "grant_type" => "refresh_token",
+            "refresh_token" => $refresh_token,
+            "client_id" => $this->client_id,
+            "client_secret" => $this->client_secret
+        ]);
+
+        if ($response && $response->status == "ok") {
+            $this->setTokens($response->access_token, $response->refresh_token);
+
+            // Call the refresh callback function if available
+            if (isset($this->refresh_callback) && is_callable($this->refresh_callback)) {
+                call_user_func_array($this->refresh_callback, 
+                    array($response->access_token, $response->refresh_token));
+            }
         }
 
         return $response;
